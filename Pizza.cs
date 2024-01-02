3cc6ec0364e8a851625926b5471598d94696b9af
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-02 10:51:45 -0800

    setup mode and class

---
+++ Pizza.cs
@@ -0,0 +1,9 @@
+// create a model made up of entity classes and context obj that reps session with db (allows query and save data)
+namespace PizzaStore.Models{
+    // class that represents a pizza
+    public class Pizza{
+        public int Id {get;set;}
+        public string? Name {get;set;}
+        public string? Description {get;set;}
+    }
+}
