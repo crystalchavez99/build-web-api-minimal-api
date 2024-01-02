66431b2893441e81f2f1f61c4a30ea120b778f9b
crystalchavez99 <chavezcrystal1999@yahoo.com>
2024-01-02 10:33:54 -0800

    create in memory store and routes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using PizzaStore.DB;
 using Microsoft.OpenApi.Models;
+
 
 
 // you create a builder, from there you create app instance
@@ -20,8 +22,21 @@ app.UseSwaggerUI(c =>
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 // app instance has routing setup and can add middle
-app.MapGet("/", () => "Hello World!");
+// get: fetch a resource just route
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
 
+// get: use route param
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+
+// post: create resource product is sent into the lambda that handles the request.
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+
+// put: update resource
+// should send a posted body with a resource that contains changes. You want these changes applied to an existing resource on the server
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+
+// delete: remove resource
+app.MapDelete("/products/{id}", (int id) => PizzaDB.RemovePizza(id));
 // Starts API
 app.Run();
 
